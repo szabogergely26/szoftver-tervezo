ef63c899a98732a2e16360016a135f2b46e225db
Gergely Szabó <szabogergely26@gmail.com>
2026-08-17 09:59:45 +0200

    Verzió 0.5.0, BUILD_CHANNEL: preview

--- version_info.py
+++ version_info.py
@@ -17,5 +17,5 @@ a hozzá tartozó apró javítások (lint, set_task_done fix) pedig ugyanannak a
 A 0.4.1 csak akkor jönne, ha a 0.4.0 kiadása UTÁN derülne ki egy hiba, amit gyorsan javítani kell.
 """
 
-APP_VERSION = "0.4.1"
-BUILD_CHANNEL = "main"  # "dev" | "preview" | "main"
+APP_VERSION = "0.5.0"
+BUILD_CHANNEL = "preview"  # "dev" | "preview" | "main"
