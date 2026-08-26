a548e0850b1802e5c245466dcc9447cd0fcf1bc0
Szabó <szabogergely26@gmail.com>
2026-08-26 18:35:40 +0200

    Verzióemelés: 0.7.1

--- version_info.py
+++ version_info.py
@@ -19,5 +19,5 @@ patch (0.3.0 → 0.3.1): javítás, bugfix, apró finomítás — pl. a set_task
 
 """
 
-APP_VERSION = "0.6.1"
+APP_VERSION = "0.7.1"
 BUILD_CHANNEL = "preview"  # "dev" | "preview" | "main"
